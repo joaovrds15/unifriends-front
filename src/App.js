11c95ff1f0570bbbcbd670d6c5b41dd88e74098b
Joao Victor Rodrigues <joaovictorodrigues4009@gmail.com>
2025-06-21 22:13:33 -0300

    feat(save-user-answers): save users answer and private routes

--- src/App.js
+++ src/App.js
@@ -6,31 +6,56 @@ import SignUpPage from './components/SignUpPage';
 import ProfilePage from './components/ProfilePage';
 import AffinityScreen from './components/AffinityScreen';
 import EmailPage from './components/EmailPage';
+import AffinityQuiz from './components/AffinityQuiz';
 import VerificationCodePage from './components/VerificationCodePage';
 import UploadImagesPage from './components/UploadImagesPage';
+import PrivateRoute from './components/PrivateRoute';
 import { UserProvider } from './context/UserContext';
-
 
 function App() {
   const [emailVerified, setEmailVerified] = useState(false);
 
-
   return (
     <UserProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route 
-          path="/signup" 
-          element={<SignUpPage setEmailVerified={setEmailVerified} />}
-        />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/affinity" element={<AffinityScreen />} />
-        <Route path="/signup/email" element={<EmailPage />} />
-        <Route path="/signup/verification" element={<VerificationCodePage />} />
-        <Route path="/upload-images" element={<UploadImagesPage />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup/email" element={<EmailPage />} />
+          <Route path="/signup/verification" element={<VerificationCodePage />} />
+          <Route 
+            path="/signup" 
+            element={<SignUpPage setEmailVerified={setEmailVerified} />}
+          />
+          <Route path="/upload-images" element={<UploadImagesPage />} />
+          
+          {/* Private routes - require authentication */}
+          <Route 
+            path="/profile" 
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/affinity-quiz" 
+            element={
+              <PrivateRoute>
+                <AffinityQuiz />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/affinity" 
+            element={
+              <PrivateRoute>
+                <AffinityScreen />
+              </PrivateRoute>
+            } 
+          />
+        </Routes>
+      </Router>
     </UserProvider>
   );
 }
